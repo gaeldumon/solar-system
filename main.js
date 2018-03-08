9e1a2e1f96e6bd17b845ec8a8009b84be9a08f8b
hyperdestru <slotuser@gmail.com>
2018-03-08 15:38:35 +0100

    Animated all planets, no more pluto

--- main.js
+++ main.js
@@ -20,9 +20,10 @@ window.onload = function () {
 
   loadJSON(function (response) {
 
-    // We parse JSON data as a js object (whole solar system w/ sun, planets and moons)
+    // We parse JSON data as an object (whole solar system)
     var solarSystemData = JSON.parse(response);
 
+    // We get the solar system elements from the data
     var sun = solarSystemData.sun;
     var mercury = solarSystemData.mercury;
     var venus = solarSystemData.venus;
@@ -32,21 +33,8 @@ window.onload = function () {
     var saturn = solarSystemData.saturn;
     var uranus = solarSystemData.uranus;
     var neptune = solarSystemData.neptune;
-    var pluto = solarSystemData.pluto;
 
-    var drawSun = function () {
-      sun.posX = canvas.width / 2;
-      sun.posY = canvas.height / 2;
-
-      ctx.beginPath();
-      ctx.fillStyle = sun.color;
-      ctx.arc(sun.posX, sun.posY, sun.sizePx, 0, Math.PI * 2, true);
-      ctx.fill();
-      ctx.closePath();
-    }
-
-    /** draw() draws space objects (spheres) on canvas, it takes care of pathing, filling, and shaping */
-    var drawPlanet = function (obj) {
+    var draw = function (obj) {
       ctx.beginPath();
       ctx.fillStyle = obj.color;
       ctx.arc(obj.posX, obj.posY, obj.sizePx, 0, Math.PI * 2, true);
@@ -54,20 +42,62 @@ window.onload = function () {
       ctx.closePath();
     }
 
-    var angle = 0;
-    var da = 0.07;
+    sun.posX = canvas.width / 2;
+    sun.posY = canvas.height / 2;
 
-    var animate = function (obj) {
-      obj = mercury;
+    var angleM = 0;
+    var angleV = 0;
+    var angleE = 0;
+    var angleMa = 0;
+    var angleJ = 0;
+    var angleS = 0;
+    var angleU = 0;
+    var angleN = 0;
 
+    var animate = function () {
       ctx.clearRect(0, 0, canvas.width, canvas.height);
+      draw(sun);
 
-      drawSun();
+      angleM = angleM + mercury.revRatio;
+      mercury.posX = sun.posX + (sun.sizePx + mercury.distSunPx) * Math.sin(angleM);
+      mercury.posY = sun.posY + (sun.sizePx + mercury.distSunPx) * Math.cos(angleM);
 
-      angle = angle + da;
-      obj.posX = sun.posX + (sun.sizePx + obj.distSunPx) * Math.sin(angle);
-      obj.posY = sun.posY + (sun.sizePx + obj.distSunPx) * Math.cos(angle);
-      drawPlanet(obj);
+      angleV = angleV + venus.revRatio;
+      venus.posX = sun.posX + (sun.sizePx + venus.distSunPx) * Math.sin(angleV);
+      venus.posY = sun.posY + (sun.sizePx + venus.distSunPx) * Math.cos(angleV);
+
+      angleE = angleE + earth.revRatio;
+      earth.posX = sun.posX + (sun.sizePx + earth.distSunPx) * Math.sin(angleE);
+      earth.posY = sun.posY + (sun.sizePx + earth.distSunPx) * Math.cos(angleE);
+
+      angleMa = angleMa + mars.revRatio;
+      mars.posX = sun.posX + (sun.sizePx + mars.distSunPx) * Math.sin(angleMa);
+      mars.posY = sun.posY + (sun.sizePx + mars.distSunPx) * Math.cos(angleMa);
+
+      angleJ = angleJ + jupiter.revRatio;
+      jupiter.posX = sun.posX + (sun.sizePx + jupiter.distSunPx) * Math.sin(angleJ);
+      jupiter.posY = sun.posY + (sun.sizePx + jupiter.distSunPx) * Math.cos(angleJ);
+
+      angleS = angleS + saturn.revRatio;
+      saturn.posX = sun.posX + (sun.sizePx + saturn.distSunPx) * Math.sin(angleS);
+      saturn.posY = sun.posY + (sun.sizePx + saturn.distSunPx) * Math.cos(angleS);
+
+      angleU = angleU + uranus.revRatio;
+      uranus.posX = sun.posX + (sun.sizePx + uranus.distSunPx) * Math.sin(angleU);
+      uranus.posY = sun.posY + (sun.sizePx + uranus.distSunPx) * Math.cos(angleU);
+
+      angleN = angleN + neptune.revRatio;
+      neptune.posX = sun.posX + (sun.sizePx + neptune.distSunPx) * Math.sin(angleN);
+      neptune.posY = sun.posY + (sun.sizePx + neptune.distSunPx) * Math.cos(angleN);
+
+      draw(mercury);
+      draw(venus);
+      draw(earth);
+      draw(mars);
+      draw(jupiter);
+      draw(saturn);
+      draw(uranus);
+      draw(neptune);
 
       window.requestAnimationFrame(animate);
     }
