3f4af3790e34c3835e272b49891e508e85e186bb
hyperdestru <slotuser@gmail.com>
2018-08-26 22:25:39 +0200

    Parent class spaceobjects and subclass planet created, some impl. of draw, animate...

--- main.js
+++ main.js
@@ -1,14 +1,52 @@
-class Planet {
-  set(name) {
-    this.name = name;
-  }
+window.onload = function () {
+  let canvas = document.getElementById('solar-canvas');
+  let context = canvas.getContext('2d');
 
-  get() {
-    console.log(this.name);
+  if (!canvas) {
+    alert("Impossible to get canvas");
+    return 1;
+  }
+  if (!ctx) {
+    alert("Impossible to get canvas context");
+    return 1;
   }
 }
 
-let arr_planet = []; //just push planets into this array
-let mercury = new Planet;
-mercury.set("Mercury");
-mercury.get();
+class SpaceObject {
+
+  SpaceObject(name, color, posX, posY, sizePx) {
+    this.name = name;
+    this.color = color;
+    this.posX = posX;
+    this.posY = posY;
+    this.sizePx = sizePx;
+  }
+
+  draw(context) {
+    context.beginPath();
+    context.fillStyle = this.color;
+    context.arc(this.posX, this.posY, this.sizePx, 0, Math.PI * 2, true);
+    context.fill();
+    context.closePath();
+  }
+}
+
+class Planet extends SpaceObject {
+
+  Planet(name, color, posX, posY, sizePx, orbitRatio, angle, distSun, distScale) {
+    super(name, color, posX, posY, sizePx);
+    this.orbitRatio = orbitRatio;
+    this.angle = angle;
+    this.distSun = distSun;
+    this.distScale = distScale;
+  }
+
+  animate() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    this.angle += this.orbitRatio;
+    this.posX = sun.posX + (sun.sizePx / 2 + this.sizePx / 2 + this.distSun * distScale) * Math.sin(angleMe);
+    this.posY = sun.posY + (sun.sizePx / 2 + this.sizePx / 2 + this.distSun * distScale) * Math.cos(angleMe);
+
+    window.requestAnimationFrame(animate);
+  }
+}
