baac874a882d644ff9346e4bfe61d26c5a8f587c
hyperdestru <slotuser@gmail.com>
2018-10-18 20:51:08 +0200

    Planet and Star classes, instanciations of sun, mercury, venus. Draw methods ok. Animate function ok. Loop through planets array ok

--- main.js
+++ main.js
@@ -1,50 +1,77 @@
 window.onload = function () {
-  let canvas = document.getElementById('solar-canvas');
-  let context = canvas.getContext('2d');
+
+  var canvas = document.getElementById('solar-canvas');
+  var ctx = canvas.getContext('2d');
 
   if (!canvas) {
     alert("Impossible to get canvas");
-    return 1;
+    return;
   }
+
   if (!ctx) {
     alert("Impossible to get canvas context");
-    return 1;
-  }
-}
-
-class SpaceObject {
-  SpaceObject(name, color, posX, posY, sizePx) {
-    this.name = name;
-    this.color = color;
-    this.posX = posX;
-    this.posY = posY;
-    this.sizePx = sizePx;
+    return;
   }
 
-  draw(context) {
-    context.beginPath();
-    context.fillStyle = this.color;
-    context.arc(this.posX, this.posY, this.sizePx, 0, Math.PI * 2, true);
-    context.fill();
-    context.closePath();
+  class Planet {
+    constructor(name, color, sizePx, distScale, posX, posY, revRatio, angle) {
+      this.name = name;
+      this.color = color;
+      this.sizePx = sizePx;
+      this.distScale = distScale;
+      this.posX = posX;
+      this.posY = posY;
+      this.revRatio = revRatio;
+      this.angle = angle;
+    }
+
+    draw() {
+      ctx.beginPath();
+      ctx.fillStyle = this.color;
+      ctx.arc(this.posX, this.posY, this.sizePx, 0, Math.PI * 2, true);
+      ctx.fill();
+      ctx.closePath();
+    }
   }
-}
 
-class Planet extends SpaceObject {
-  Planet(name, color, posX, posY, sizePx, orbitRatio, angle, distSun, distScale) {
-    super(name, color, posX, posY, sizePx);
-    this.orbitRatio = orbitRatio;
-    this.angle = angle;
-    this.distSun = distSun;
-    this.distScale = distScale;
+  class Star {
+    constructor(name, color, sizePx, posX, posY) {
+      this.name = name;
+      this.color = color;
+      this.sizePx = sizePx;
+      this.posX = posX;
+      this.posY = posY;
+    }
+
+    draw() {
+      ctx.beginPath();
+      ctx.fillStyle = this.color;
+      ctx.arc(this.posX, this.posY, this.sizePx, 0, Math.PI * 2, true);
+      ctx.fill();
+      ctx.closePath();
+    }
   }
 
-  animate() {
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    this.angle += this.orbitRatio;
-    this.posX = sun.posX + (sun.sizePx / 2 + this.sizePx / 2 + this.distSun * distScale) * Math.sin(angleMe);
-    this.posY = sun.posY + (sun.sizePx / 2 + this.sizePx / 2 + this.distSun * distScale) * Math.cos(angleMe);
+  let sun = new Star("Sun", "yellow", 40, canvas.width / 2, canvas.height / 2);
+  let mercury = new Planet("Mercury", "white", 2, 59, sun.posX + this.distScale, canvas.height / 2, 0.055, 0);
+  let venus = new Planet("Venus", "#DE5F25", 4.85, 90, sun.posX + this.distScale, canvas.height / 2, 0.022, 0);
 
+  let planets = [];
+
+  planets.push(mercury);
+  planets.push(venus);
+
+  var animate = function () {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    sun.draw();
+
+    planets.forEach(function (e) {
+      e.angle += e.revRatio;
+      e.posX = sun.posX + e.distScale * Math.sin(e.angle);
+      e.posY = sun.posY + e.distScale * Math.cos(e.angle);
+      e.draw();
+    });
     window.requestAnimationFrame(animate);
   }
+  window.requestAnimationFrame(animate);
 }
