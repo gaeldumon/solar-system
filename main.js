c641ec3c61d9e16a44def05791f1968589449235
hyperdestru <slotuser@gmail.com>
2018-10-19 18:28:25 +0200

    drawing/animating all planets, displaying of earth posX/Y data

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 window.onload = function () {
 
   var canvas = document.getElementById('solar-canvas');
-  var ctx = canvas.getContext('2d');
+  var context = canvas.getContext('2d');
 
   if (!canvas) {
     alert("Impossible to get canvas");
     return;
   }
 
-  if (!ctx) {
+  if (!context) {
     alert("Impossible to get canvas context");
     return;
   }
@@ -26,11 +26,11 @@ window.onload = function () {
     }
 
     draw() {
-      ctx.beginPath();
-      ctx.fillStyle = this.color;
-      ctx.arc(this.posX, this.posY, this.sizePx, 0, Math.PI * 2, true);
-      ctx.fill();
-      ctx.closePath();
+      context.beginPath();
+      context.fillStyle = this.color;
+      context.arc(this.posX, this.posY, this.sizePx, 0, Math.PI * 2, true);
+      context.fill();
+      context.closePath();
     }
   }
 
@@ -44,22 +44,30 @@ window.onload = function () {
     }
 
     draw() {
-      ctx.beginPath();
-      ctx.fillStyle = this.color;
-      ctx.arc(this.posX, this.posY, this.sizePx, 0, Math.PI * 2, true);
-      ctx.fill();
-      ctx.closePath();
+      context.beginPath();
+      context.fillStyle = this.color;
+      context.arc(this.posX, this.posY, this.sizePx, 0, Math.PI * 2, true);
+      context.fill();
+      context.closePath();
     }
   }
 
   let sun = new Star("Sun", "yellow", 40, canvas.width / 2, canvas.height / 2);
-  let mercury = new Planet("Mercury", "white", 2, 59, sun.posX + this.distScale, canvas.height / 2, 0.055, 0);
+  let mercury = new Planet("Mercury", "white", 1.85, 59, sun.posX + this.distScale, canvas.height / 2, 0.055, 0);
   let venus = new Planet("Venus", "#DE5F25", 4.85, 90, sun.posX + this.distScale, canvas.height / 2, 0.022, 0);
+  let earth = new Planet("Earth", "blue", 5.10, 110, sun.posX + this.distScale, canvas.height / 2, 0.013, 0);
+  let mars = new Planet("Mars", "red", 3, 140, sun.posX + this.distScale, canvas.height / 2, 0.006, 0);
+  let jupiter = new Planet("Jupiter", "orange", 20.42, 220, sun.posX + this.distScale, canvas.height / 2, 0.0015, 0);
+  let saturn = new Planet("Saturn", "#A88B6D", 17.23, 320, sun.posX + this.distScale, canvas.height / 2, 0.0010, 0);
+  let uranus = new Planet("Uranus", "#9FC4CA", 7.30, 400, sun.posX + this.distScale, canvas.height / 2, 0.0003, 0);
+  let neptune = new Planet("Neptune", "#3454DF", 7.07, 450, sun.posX + this.distScale, canvas.height / 2, 0.0006, 0);
 
-  let planets = [mercury, venus];
+  let planets = [mercury, venus, earth, mars, jupiter, saturn, uranus, neptune];
+  var display_earthPosX = document.getElementById("display_earthPosX");
+  var display_earthPosY = document.getElementById("display_earthPosY");
 
   var animate = function () {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    context.clearRect(0, 0, canvas.width, canvas.height);
     sun.draw();
 
     planets.forEach(function (e) {
@@ -68,6 +76,10 @@ window.onload = function () {
       e.posY = sun.posY + e.distScale * Math.cos(e.angle);
       e.draw();
     });
+
+    display_earthPosX.innerHTML = Math.trunc(earth.posX);
+    display_earthPosY.innerHTML = Math.trunc(earth.posY);
+
     window.requestAnimationFrame(animate);
   }
   window.requestAnimationFrame(animate);
