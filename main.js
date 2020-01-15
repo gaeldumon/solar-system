600143b3f77113b5ce90ef96c44c1e1f51bc27af
= <slotuser@gmail.com>
2020-01-15 15:15:20 +0100

    Wrote just one object for both planets and moons + implemt of earth moon.

--- main.js
+++ main.js
@@ -10,6 +10,34 @@ if (!context) {
 	console.log("Impossible to get canvas context");
 }
 
+class Astra {
+	constructor(name, color, radius, dist, angSpeed, parent) {
+		this.name = name;
+		this.color = color;
+		this.radius = radius;
+		this.dist = dist;
+		this.angSpeed = angSpeed;
+		this.parent = parent;
+		this.x = this.parent.x + this.dist;
+		this.y = this.parent.y;
+		this.angle = 0;
+	}
+
+	update() {
+		this.angle += this.angSpeed;
+		this.x = this.parent.x + this.dist * Math.sin(this.angle);
+		this.y = this.parent.y + this.dist * Math.cos(this.angle);
+	}
+
+	draw() {
+		context.beginPath();
+		context.fillStyle = this.color;
+		context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+		context.fill();
+		context.closePath();
+	}
+}
+
 class Star {
 	constructor(name, color, radius, x, y) {
 		this.name = name;
@@ -28,46 +56,22 @@ class Star {
 	}
 }
 
-class Planet {
-	constructor(name, color, radius, distScale, revRatio, star) {
-		this.name = name;
-		this.color = color;
-		this.radius = radius;
-		this.distScale = distScale;
-		this.parentStar = star
-		this.x = this.parentStar.x + this.distScale;
-		this.y = this.parentStar.y;
-		this.revRatio = revRatio;
-		this.angle = 0;
-	}
-
-	update() {
-		this.angle += this.revRatio;
-		this.x = this.parentStar.x + this.distScale * Math.sin(this.angle);
-		this.y = this.parentStar.y + this.distScale * Math.cos(this.angle);
-	}
-
-	draw() {
-		context.beginPath();
-		context.fillStyle = this.color;
-		context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
-		context.fill();
-		context.closePath();
-	}
-}
-
 let sun = new Star("Sun", "yellow", 40, canvas.width/2, canvas.height/2);
-let mercury = new Planet("Mercury", "#ffffff", 1.85, 59, 0.055, sun);
-let venus = new Planet("Venus", "#de5f25", 4.85, 90, 0.022, sun);
-let earth = new Planet("Earth", "blue", 5.10, 110, 0.013, sun);
-let mars = new Planet("Mars", "red", 3, 140, 0.006, sun);
-let jupiter = new Planet("Jupiter", "orange", 20.42, 220, 0.0015, sun);
-let saturn = new Planet("Saturn", "#a88b6d", 17.23, 320, 0.0010, sun);
-let uranus = new Planet("Uranus", "#9fc4ca", 7.30, 400, 0.0003, sun);
-let neptune = new Planet("Neptune", "#3454df", 7.07, 450, 0.0006, sun);
+
+let mercury = new Astra("Mercury", "#ffffff", 1.85, 59, 0.055, sun);
+let venus = new Astra("Venus", "#de5f25", 4.85, 90, 0.022, sun);
+let earth = new Astra("Earth", "blue", 5.10, 110, 0.013, sun);
+let mars = new Astra("Mars", "red", 3, 140, 0.006, sun);
+let jupiter = new Astra("Jupiter", "orange", 20.42, 220, 0.0015, sun);
+let saturn = new Astra("Saturn", "#a88b6d", 17.23, 320, 0.0010, sun);
+let uranus = new Astra("Uranus", "#9fc4ca", 7.30, 400, 0.0003, sun);
+let neptune = new Astra("Neptune", "#3454df", 7.07, 450, 0.0006, sun);
+
+let moon = new Astra('Moon', 'white', 1, 10, 0.08, earth);
 
 let planets = [mercury, venus, earth, mars, jupiter, saturn, uranus, neptune];
 let stars = [sun];
+let moons = [moon];
 
 function clear() {
 	context.clearRect(0, 0, canvas.width, canvas.height);
@@ -84,6 +88,11 @@ function mainUpdate() {
 	planets.forEach(function(planet) {
 		planet.update();
 		planet.draw();
+	});
+
+	moons.forEach(function(moon) {
+		moon.update();
+		moon.draw();
 	});
 
 	stopUpdate = requestAnimationFrame(mainUpdate);
