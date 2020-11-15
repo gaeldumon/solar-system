488a03d7fcf7342c7ec696ae0b0b8529b34e59fd
= <slotuser@gmail.com>
2020-11-15 17:51:19 +0100

    Prettier format.

--- src/main.js
+++ src/main.js
@@ -1,155 +1,136 @@
-const canvas = document.getElementById('solar-canvas')
-const context = canvas.getContext('2d', { alpha: false })
-let stopUpdate
+/** @format */
 
-if (!canvas) 
-	console.log("Impossible to get canvas")
-if (!context) 
-	console.log("Impossible to get canvas context")
+const canvas = document.getElementById("solar-canvas");
+const context = canvas.getContext("2d", { alpha: false });
+let stopUpdate;
 
-class SpaceObject 
-{
-	constructor(name, color, radius, x, y) 
-	{
-		this.name = name
-		this.color = color
-		this.radius = radius
-		this.x = x
-		this.y = y
-	}
+if (!canvas) console.log("Impossible to get canvas");
+if (!context) console.log("Impossible to get canvas context");
 
-	draw() 
-	{
-		context.beginPath()
-		context.fillStyle = this.color
-		context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true)
-		context.fill()
-		context.closePath()
-	}
+class SpaceObject {
+    constructor(name, color, radius, x, y) {
+        this.name = name;
+        this.color = color;
+        this.radius = radius;
+        this.x = x;
+        this.y = y;
+    }
+
+    draw() {
+        context.beginPath();
+        context.fillStyle = this.color;
+        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+        context.fill();
+        context.closePath();
+    }
 }
 
-class Star extends SpaceObject 
-{
-	constructor(name, color, radius, x, y) 
-	{
-		super(name, color, radius, x, y)
-	}
+class Star extends SpaceObject {
+    constructor(name, color, radius, x, y) {
+        super(name, color, radius, x, y);
+    }
 
-	draw() 
-	{
-		super.draw()
-	}
+    draw() {
+        super.draw();
+    }
 }
 
-class Planet extends SpaceObject 
-{
-	constructor(name, color, radius, dist, angSpeed, parent, rings = false) 
-	{
-		super(name, color, radius)
-		this.dist = dist
-		this.angSpeed = angSpeed
-		this.parent = parent
-		this.x = this.parent.x + this.dist
-		this.y = this.parent.y
-		this.angle = 0
-		this.rings = rings
-	}
+class Planet extends SpaceObject {
+    constructor(name, color, radius, dist, angSpeed, parent, rings = false) {
+        super(name, color, radius);
+        this.dist = dist;
+        this.angSpeed = angSpeed;
+        this.parent = parent;
+        this.x = this.parent.x + this.dist;
+        this.y = this.parent.y;
+        this.angle = 0;
+        this.rings = rings;
+    }
 
-	update() 
-	{
-		this.angle += this.angSpeed
-		this.x = this.parent.x + this.dist * Math.sin(this.angle)
-		this.y = this.parent.y + this.dist * Math.cos(this.angle)
-	}
+    update() {
+        this.angle += this.angSpeed;
+        this.x = this.parent.x + this.dist * Math.sin(this.angle);
+        this.y = this.parent.y + this.dist * Math.cos(this.angle);
+    }
 
-	drawRing() 
-	{
-		context.fillStyle = '#ffffff'
-		context.beginPath()
-		context.ellipse(
-			this.x, 
-			this.y, 
-			this.radius+5, 
-			1, 
-			Math.PI * -0.15, 
-			0, 
-			Math.PI * 2, 
-			true
-		)
-		context.closePath()
-		context.fill()
-	}
+    drawRing() {
+        context.fillStyle = "#ffffff";
+        context.beginPath();
+        context.ellipse(
+            this.x,
+            this.y,
+            this.radius + 5,
+            1,
+            Math.PI * -0.15,
+            0,
+            Math.PI * 2,
+            true
+        );
+        context.closePath();
+        context.fill();
+    }
 
-	draw()
-	{
-		super.draw()
-		if (this.rings) 
-			this.drawRing()
-	}
+    draw() {
+        super.draw();
+        if (this.rings) this.drawRing();
+    }
 }
 
-class Moon extends Planet 
-{
-	constructor(name, color, radius, dist, angSpeed, parent) 
-	{
-		super(name, color, radius, dist, angSpeed, parent)
-	}
+class Moon extends Planet {
+    constructor(name, color, radius, dist, angSpeed, parent) {
+        super(name, color, radius, dist, angSpeed, parent);
+    }
 
-	update() 
-	{
-		super.update()
-	}
+    update() {
+        super.update();
+    }
 
-	drawRing() 
-	{
-		super.drawRing()
-	}
+    drawRing() {
+        super.drawRing();
+    }
 
-	draw() 
-	{
-		super.draw()
-	}
+    draw() {
+        super.draw();
+    }
 }
 
-const sun 		= new Star		("Sun", 	"yellow", 	40, 	canvas.width/2, canvas.height/2)
-const mercury 	= new Planet	("Mercury", "#ffffff", 	1.85, 	59, 	0.055, 	sun)
-const venus 	= new Planet	("Venus", 	"#de5f25", 	4.85, 	90, 	0.022, 	sun)
-const earth 	= new Planet	("Earth", 	"blue", 	5.10, 	110, 	0.013, 	sun)
-const mars 		= new Planet	("Mars", 	"red", 		3.00, 	140, 	0.006, 	sun)
-const jupiter 	= new Planet	("Jupiter", "orange", 	20.42, 	220, 	0.0015, sun)
-const saturn 	= new Planet	("Saturn", 	"#a88b6d", 	17.23, 	320, 	0.0010, sun, true)
-const uranus 	= new Planet	("Uranus", 	"#9fc4ca", 	7.30, 	400, 	0.0003, sun)
-const neptune 	= new Planet	("Neptune", "#3454df", 	7.07, 	450, 	0.0006, sun)
-const moon 		= new Moon		("Moon", 	"#ffffff", 	1.00, 	10, 	0.013, 	earth)
+const sun = new Star("Sun", "yellow", 40, canvas.width / 2, canvas.height / 2);
+const mercury = new Planet("Mercury", "#ffffff", 1.85, 59, 0.055, sun);
+const venus = new Planet("Venus", "#de5f25", 4.85, 90, 0.022, sun);
+const earth = new Planet("Earth", "blue", 5.1, 110, 0.013, sun);
+const mars = new Planet("Mars", "red", 3.0, 140, 0.006, sun);
+const jupiter = new Planet("Jupiter", "orange", 20.42, 220, 0.0015, sun);
+const saturn = new Planet("Saturn", "#a88b6d", 17.23, 320, 0.001, sun, true);
+const uranus = new Planet("Uranus", "#9fc4ca", 7.3, 400, 0.0003, sun);
+const neptune = new Planet("Neptune", "#3454df", 7.07, 450, 0.0006, sun);
+const moon = new Moon("Moon", "#ffffff", 1.0, 10, 0.013, earth);
 
-const planets = [ mercury, venus, earth, mars, jupiter, saturn, uranus, neptune ]
-const stars = [ sun ]
-const moons = [ moon ]
+const planets = [mercury, venus, earth, mars, jupiter, saturn, uranus, neptune];
+const stars = [sun];
+const moons = [moon];
 
-function clear() 
-{
-	context.clearRect(0, 0, canvas.width, canvas.height)
+function clear() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
 }
 
-function mainUpdate() 
-{
-	clear()
+function mainUpdate() {
+    clear();
 
-	stars.forEach(function(star) {
-		star.draw()
-	})
+    stars.forEach(function (star) {
+        star.draw();
+    });
 
-	planets.forEach(function(planet) {
-		planet.update()
-		planet.draw()
-	})
+    planets.forEach(function (planet) {
+        planet.update();
+        planet.draw();
+    });
 
-	moons.forEach(function(moon) {
-		moon.update()
-		moon.draw()
-	})
+    moons.forEach(function (moon) {
+        moon.update();
+        moon.draw();
+    });
 
-	stopUpdate = requestAnimationFrame(mainUpdate)
+    stopUpdate = requestAnimationFrame(mainUpdate);
 }
 
-mainUpdate()
+mainUpdate();
